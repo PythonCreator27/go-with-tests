333892eb9e0000e8602e3ca3620e6fd9689f6fc2
advaiyalad <advaiya.lad@gmail.com>
2021-04-28 11:47:44 -0500

    feat!(iteration): custom repeat count

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
+	repeated := Repeat("a", 5)
 	expected := "aaaaa"
 
 	if repeated != expected {
@@ -16,12 +16,12 @@ func TestRepeat(t *testing.T) {
 
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", 5)
 	}
 }
 
 func ExampleRepeat() {
-	repeated := Repeat("ABC")
+	repeated := Repeat("ABC", 2)
 	fmt.Println(repeated)
-	// Output: ABCABCABCABCABC
+	// Output: ABCABC
 }
